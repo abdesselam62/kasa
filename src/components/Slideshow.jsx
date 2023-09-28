1cd9e618839883f0f571de06a953897a25a4a7c6
abdesselam62 <slamusdr@hotmail.com>
2023-09-28 21:12:34 +0200

    erreur corrigée slideshow

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -3,46 +3,40 @@ import chevronLeft from "../assets/chevron_left.png";
 import chevronRight from "../assets/chevron_right.png";
 import "../styles/Slideshow.css";
 
-export default function Slider({imageSlider}) {
-    const [currentIndex, setCurrentIndex] = useState(0); //je définis l'index du premier slide à 0
-    
-    const nextSlide = () => {
-        setCurrentIndex(currentIndex + 1);
-        if (currentIndex === imageSlider.length - 1) setCurrentIndex(0); // on repart au premier slide quand on arrive au dernier
-    };
-    const prevSlide = () => {
-        setCurrentIndex(currentIndex - 1)
-        if (currentIndex === 0) setCurrentIndex(imageSlider.length - 1); // on repart au dernier slide quand on est au premier
-    };
+function Slider({ imageSlider }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
 
+  const nextSlide = () => {
+      setCurrentIndex(currentIndex === imageSlider.length - 1 ? 0 : currentIndex + 1);
+  }; // Si l'image actuelle est la dernière, on revient à la première, sinon on passe à l'image suivante
+
+  const prevSlide = () => {
+      setCurrentIndex(currentIndex === 0 ? imageSlider.length - 1 : currentIndex - 1);
+  }; // Si l'image actuelle est la première, on revient à la dernière, sinon on passe à l'image précédente
+
+  if (!Array.isArray(imageSlider) || imageSlider.length <= 0) {
+      return null;
+  } // Si le tableau est vide, on ne retourne rien.
 
   return (
-    <div className="slideShow">
-      <div className="slideShow_img">
-        <img src={imageSlider[currentIndex]} alt="image_lodging" />
+      <div className="slideShow">
+          <div className="slideShow_img">
+              <img key={imageSlider.id} className="slideShow_img" src={imageSlider[currentIndex]} alt={imageSlider.title}/>
+          </div>
+          <div className="slideShow_number">
+              {currentIndex + 1}/{imageSlider.length}
+          </div>
+          <div>
+              {imageSlider.length > 1 && ( // Si il y a plus d'une image, on affiche les chevrons
+                  <>
+                  <img className="chevron_left" src={chevronLeft} onClick={prevSlide} alt="chevron_left"/>
+                  <img className="chevron_right" src={chevronRight} onClick={nextSlide} alt="chevron_right"/>
+                  </>
+              )}
+          </div>
       </div>
-      <div>
-        <div>
-          <img
-            src={chevronLeft}
-            alt="chevron_left"
-            className={imageSlider.length <= 1 ? "chevron_none" : "chevron_left"}
-            onClick={prevSlide}
-          />
-        </div>
-        <div className="slideShow_number">
-          {currentIndex + 1}/{imageSlider.length}
-        </div>
-        <div>
-          <img
-            src={chevronRight}
-            alt="chevron_right"
-            className={imageSlider.length <= 1 ? "chevron_none" : "chevron_right"}
-            onClick={nextSlide}
-          />
-        </div>
-      </div>
-    </div>
-  );
+  )
 }
 
+export default Slider;
